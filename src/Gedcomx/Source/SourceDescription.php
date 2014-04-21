7ee0d405113e4d0ef9e23110b8fe8fcbb3d18941
Ryan Heaton <ryan@webcohesion.com>
2014-04-21 11:53:50 -0600

    fixup the model update
    Conflicts:
    
    	src/Gedcomx/Agent/Agent.php
    	src/Gedcomx/Conclusion/Subject.php
    	src/Gedcomx/Source/SourceDescription.php

--- src/Gedcomx/Source/SourceDescription.php
+++ src/Gedcomx/Source/SourceDescription.php
@@ -683,7 +683,10 @@ class SourceDescription extends \Gedcomx\Links\HypermediaEnabledData
         if ($this->identifiers) {
             $ab = array();
             foreach ($this->identifiers as $i => $x) {
-                $ab[$i] = $x->toArray();
+                $ab[$i] = array();
+                foreach ($x as $j => $y) {
+                    $ab[$i][$j] = $y->getValue();
+                }
             }
             $a['identifiers'] = $ab;
         }
@@ -792,7 +795,16 @@ class SourceDescription extends \Gedcomx\Links\HypermediaEnabledData
         $this->identifiers = array();
         if (isset($o['identifiers'])) {
             foreach ($o['identifiers'] as $i => $x) {
-                $this->identifiers[$i] = new \Gedcomx\Conclusion\Identifier($x);
+                if (is_array($x)) {
+                    $this->identifiers[$i] = array();
+                    foreach ($x as $j => $y) {
+                        $this->identifiers[$i][$j] = new \Gedcomx\Conclusion\Identifier();
+                        $this->identifiers[$i][$j]->setValue($y);
+                    }
+                }
+                else {
+                    $this->identifiers[$i] = new \Gedcomx\Conclusion\Identifier($x);
+                }
             }
         }
         if (isset($o['created'])) {
