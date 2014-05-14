3187b8b82d08e00833239da1237ba69d80de9373
Ryan Heaton <ryan@webcohesion.com>
2014-05-14 16:20:27 -0600

    adding a brief XML serialization test

--- tests/Gedcomx/Tests/SerializationTest.php
+++ tests/Gedcomx/Tests/SerializationTest.php
@@ -4,10 +4,12 @@
 namespace Gedcomx\Tests;
 
 use Gedcomx\Gedcomx;
+use Gedcomx\Records\RecordSet;
+use XMLReader;
 
 class SerializationTest extends \PHPUnit_Framework_TestCase {
 
-    public function testSerializeDeserialize()
+    public function testSerializeDeserializeJson()
     {
         $content = file_get_contents(__DIR__ . '/alma-heaton.json');
         //print $content;
@@ -16,6 +18,16 @@ class SerializationTest extends \PHPUnit_Framework_TestCase {
         //print $content_processed;
         $gx2 = new Gedcomx(json_decode($content_processed, true));
         $this->assertEquals($gx1->toArray(), $gx2->toArray());
+    }
+
+    public function testSerializeDeserializeXmlRecords()
+    {
+        $xml = new XMLReader();
+        $this->assertTrue($xml->open(__DIR__ . '/sample-gx.xml'));
+        //print $content;
+        $recordSet = new RecordSet($xml);
+        $this->assertEquals(33, sizeof($recordSet->getRecords()));
+        $this->assertEquals("#s1", $recordSet->getRecords()[0]->getDescriptionRef());
     }
 
 }
