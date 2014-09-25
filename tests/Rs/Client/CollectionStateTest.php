effe6268a54d2f8feed7e8bdcaba41c940e2e86b
Sean Hammon <sean.hammon@stgconsulting.com>
2014-09-25 16:00:39 -0700

    update Person search unit test to test both query builder and string value

--- tests/Rs/Client/CollectionStateTest.php
+++ tests/Rs/Client/CollectionStateTest.php
@@ -2,12 +2,30 @@
 
 namespace Gedcomx\Tests\Rs\Client;
 
+use Gedcomx\Rs\Client\Util\GedcomxPersonSearchQueryBuilder;
 use Gedcomx\Tests\ApiTestCase;
 use Gedcomx\Rs\Client\StateFactory;
 
 class CollectionStateTest extends ApiTestCase{
 
-	public function testSearchForPersons(){
+	public function testSearchForPersonsWithQueryBuilder(){
+		$query = new GedcomxPersonSearchQueryBuilder();
+		$query->givenName("George")
+			  ->surname("Washington")
+			  ->deathPlace( "Mount Vernon, VA" );
+		$stateFactory = new StateFactory();
+		$searchResults = $stateFactory
+			->newCollectionState($this->apiEndpoint)
+			->authenticateViaOAuth2Password(
+				$this->apiCredentials->username,
+				$this->apiCredentials->password,
+				$this->apiCredentials->apiKey)
+			->searchForPersons($query);
+
+		$this->assertNotNull($searchResults->getPerson());
+	}
+
+	public function testSearchForPersonsWithQueryString(){
 		$stateFactory = new StateFactory();
 		$persons = $stateFactory
 			->newCollectionState($this->apiEndpoint)
@@ -27,8 +45,9 @@ class CollectionStateTest extends ApiTestCase{
 				$this->apiCredentials->username,
 				$this->apiCredentials->password,
 				$this->apiCredentials->apiKey)
-			->readPerson('');
+			->readPerson('PPPJ-MYZ');
 
+		$stop = "here";
 		$this->assertNotNull($persons->getPerson());
 	}
 } 
