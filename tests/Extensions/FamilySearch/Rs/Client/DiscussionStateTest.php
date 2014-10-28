58db4b607bbce8b394a1ed9903f84486615a64ef
Sean Hammon <sean@dezynworks.com>
2014-10-28 17:21:07 -0600

    Stubs for discussion tests

---
+++ tests/Extensions/FamilySearch/Rs/Client/DiscussionStateTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Gedcomx\Tests\Extensions\FamilySearch\Rs\Client;
+
+class DiscussionStateTest {
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/discussions/Create_Discussion_usecase
+     */
+    public function testCreateDiscussion()
+    {
+
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/discussions/Create_Comment_usecase
+     */
+    public function testCreateComment()
+    {
+
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/discussions/Read_Discussion_usecase
+     */
+    public function testReadComment()
+    {
+
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/discussions/Read_Comments_usecase
+     */
+    public function testReadComments()
+    {
+
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/discussions/Update_Discussion_usecase
+     */
+    public function testUpdateDiscussion()
+    {
+
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/discussions/Update_Comment_usecase
+     */
+    public function testUpdateComment()
+    {
+
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/discussions/Delete_Discussion_usecase
+     */
+    public function testDeleteDiscussion()
+    {
+
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/discussions/Delete_Comment_usecase
+     */
+    public function testDeleteComment()
+    {
+
+    }
+} 
