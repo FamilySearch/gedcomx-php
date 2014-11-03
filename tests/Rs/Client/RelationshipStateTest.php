10b9a1d42d6a5fb86752071aefc26f02ad25662c
Sean Hammon <sean.hammon@stgconsulting.com>
2014-11-03 12:10:31 -0700

    Relationship notes tests

--- tests/Rs/Client/RelationshipStateTest.php
+++ tests/Rs/Client/RelationshipStateTest.php
@@ -3,6 +3,7 @@
 namespace Gedcomx\Tests\Rs\Client;
 
 use Gedcomx\Common\Attribution;
+use Gedcomx\Common\Note;
 use Gedcomx\Common\ResourceReference;
 use Gedcomx\Conclusion\DateInfo;
 use Gedcomx\Conclusion\Fact;
@@ -330,6 +331,10 @@ class RelationshipStateTest extends ApiTestCase
         $note = NoteBuilder::createNote();
         $updated = $relation->addNote($note);
         $this->assertAttributeEquals(HttpStatus::CREATED, "statusCode", $updated->getResponse(), $this->buildFailMessage(__METHOD__."(addSpouse)", $updated));
+
+        $relation->delete();
+        $person2->delete();
+        $person1->delete();
     }
 
     /**
@@ -380,6 +385,59 @@ class RelationshipStateTest extends ApiTestCase
         $notes = $relation->getRelationship()->getNotes();
         $noted = $relation->readNote($notes[0]);
         $this->assertAttributeEquals(HttpStatus::OK, "statusCode", $noted->getResponse(), $this->buildFailMessage(__METHOD__."(addSpouse)", $noted));
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/tree/Update_Couple_Relationship_Note_usecase
+     */
+    public function testUpdateCoupleRelationshipNote()
+    {
+        $factory = new FamilyTreeStateFactory();
+        $this->collectionState($factory);
+
+        $person1 = $this->createPerson('male')->get();
+        $person2 = $this->createPerson('female')->get();
+
+        /* Create Relationship */
+        /** @var $relation RelationshipState */
+        $relation = $this->collectionState()->addSpouseRelationship($person1, $person2)->get();
+        $this->assertAttributeEquals(HttpStatus::OK, "statusCode", $relation->getResponse(), $this->buildFailMessage(__METHOD__."(addSpouse)", $relation));
+
+        $note = NoteBuilder::createNote();
+        $noted = $relation->addNote($note);
+        $this->assertAttributeEquals(HttpStatus::CREATED, "statusCode", $noted->getResponse(), $this->buildFailMessage(__METHOD__."(addSpouse)", $noted));
+
+        $relation->loadNotes();
+        $notes = $relation->getRelationship()->getNotes();
+        $notes[0]->setText($this->faker->sentence(12));
+        $noted = $relation->updateNote($notes[0]);
+        $this->assertAttributeEquals(HttpStatus::NO_CONTENT, "statusCode", $noted->getResponse(), $this->buildFailMessage(__METHOD__."(addSpouse)", $noted));
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/tree/Update_Couple_Relationship_Note_usecase
+     */
+    public function testDeleteCoupleRelationshipNote()
+    {
+        $factory = new FamilyTreeStateFactory();
+        $this->collectionState($factory);
+
+        $person1 = $this->createPerson('male')->get();
+        $person2 = $this->createPerson('female')->get();
+
+        /* Create Relationship */
+        /** @var $relation RelationshipState */
+        $relation = $this->collectionState()->addSpouseRelationship($person1, $person2)->get();
+        $this->assertAttributeEquals(HttpStatus::OK, "statusCode", $relation->getResponse(), $this->buildFailMessage(__METHOD__."(addSpouse)", $relation));
+
+        $note = NoteBuilder::createNote();
+        $noted = $relation->addNote($note);
+        $this->assertAttributeEquals(HttpStatus::CREATED, "statusCode", $noted->getResponse(), $this->buildFailMessage(__METHOD__."(addSpouse)", $noted));
+
+        $relation->loadNotes();
+        $notes = $relation->getRelationship()->getNotes();
+        $noted = $relation->deleteNote($notes[0]);
+        $this->assertAttributeEquals(HttpStatus::NO_CONTENT, "statusCode", $noted->getResponse(), $this->buildFailMessage(__METHOD__."(addSpouse)", $noted));
     }
 
     /**
@@ -461,5 +519,138 @@ class RelationshipStateTest extends ApiTestCase
         $relation->delete();
         $person1->delete();
         $person2->delete();
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/tree/Create_Child-and-Parents_Relationship_Note_usecase
+     */
+    public function testCreateChildAndParentsRelationshipNote()
+    {
+        $factory = new FamilyTreeStateFactory();
+        $this->collectionState($factory);
+
+        $child = $this->createPerson();
+        $father = $this->createPerson('male')->get();
+        $mother = $this->createPerson('female')->get();
+
+        $relation = $this->collectionState()->addChildAndParents($child, $father, $mother);
+        $note = NoteBuilder::createNote();
+        $noted  = $relation->addNote($note);
+        $this->assertAttributeEquals(HttpStatus::CREATED, "statusCode", $noted->getResponse(), "Restore person failed. Returned {$noted->getResponse()->getStatusCode()}");
+
+        $relation->delete();
+        $child->delete();
+        $mother->delete();
+        $father->delete();
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/tree/Read_Child-and-Parents_Relationship_Notes_usecase
+     */
+    public function testReadChildAndParentsRelationshipNotes()
+    {
+        $factory = new FamilyTreeStateFactory();
+        $this->collectionState($factory);
+
+        $child = $this->createPerson();
+        $father = $this->createPerson('male')->get();
+        $mother = $this->createPerson('female')->get();
+
+        $relation = $this->collectionState()->addChildAndParents($child, $father, $mother);
+        $note = NoteBuilder::createNote();
+        $relation->addNote($note);
+        $note = NoteBuilder::createNote();
+        $relation->addNote($note);
+
+        $relation = $relation->get()->loadNotes();
+        $notes = $relation->getRelationship()->getNotes();
+        $this->assertEquals(2, count($notes));
+
+        $relation->delete();
+        $child->delete();
+        $mother->delete();
+        $father->delete();
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/tree/Read_Child-and-Parents_Relationship_Note_usecase
+     */
+    public function testReadChildAndParentsRelationshipNote()
+    {
+        $factory = new FamilyTreeStateFactory();
+        $this->collectionState($factory);
+
+        $child = $this->createPerson();
+        $father = $this->createPerson('male')->get();
+        $mother = $this->createPerson('female')->get();
+
+        $relation = $this->collectionState()->addChildAndParents($child, $father, $mother);
+        $note = NoteBuilder::createNote();
+        $relation->addNote($note);
+
+        $relation = $relation->get()->loadNotes();
+        $notes = $relation->getRelationship()->getNotes();
+        $noted = $relation->readNote($notes[0]);
+        $this->assertAttributeEquals(HttpStatus::OK, "statusCode", $noted->getResponse(), $this->buildFailMessage(__METHOD__."(addSpouse)", $noted));
+
+        $relation->delete();
+        $child->delete();
+        $mother->delete();
+        $father->delete();
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/tree/Update_Child-and-Parents_Relationship_Note_usecase
+     */
+    public function testUpdateChildAndParentsRelationshipNote()
+    {
+        $factory = new FamilyTreeStateFactory();
+        $this->collectionState($factory);
+
+        $child = $this->createPerson();
+        $father = $this->createPerson('male')->get();
+        $mother = $this->createPerson('female')->get();
+
+        $relation = $this->collectionState()->addChildAndParents($child, $father, $mother);
+        $note = NoteBuilder::createNote();
+        $relation->addNote($note);
+
+        $relation = $relation->get()->loadNotes();
+        $notes = $relation->getRelationship()->getNotes();
+        $notes[0]->setText($this->faker->sentence(12));
+        $noted = $relation->updateNote($notes[0]);
+        $this->assertAttributeEquals(HttpStatus::NO_CONTENT, "statusCode", $noted->getResponse(), $this->buildFailMessage(__METHOD__."(addSpouse)", $noted));
+
+        $relation->delete();
+        $child->delete();
+        $mother->delete();
+        $father->delete();
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/tree/Delete_Child-and-Parents_Relationship_Note_usecase
+     */
+    public function testDeleteChildAndParentRelationshipNote()
+    {
+        $factory = new FamilyTreeStateFactory();
+        $this->collectionState($factory);
+
+        $child = $this->createPerson();
+        $father = $this->createPerson('male')->get();
+        $mother = $this->createPerson('female')->get();
+
+        $relation = $this->collectionState()->addChildAndParents($child, $father, $mother);
+        $note = NoteBuilder::createNote();
+        $relation->addNote($note);
+
+        $relation = $relation->get()->loadNotes();
+        $notes = $relation->getRelationship()->getNotes();
+        $noted = $relation->deleteNote($notes[0]);
+        $this->assertAttributeEquals(HttpStatus::NO_CONTENT, "statusCode", $noted->getResponse(), $this->buildFailMessage(__METHOD__, $noted));
+
+        $relation->delete();
+        $child->delete();
+        $mother->delete();
+        $father->delete();
     }
 }
