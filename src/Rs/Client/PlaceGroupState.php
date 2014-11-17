6fb7c5b21966427ade2db85035ec6890f7d8e364
Sean Hammon <sean@dezynworks.com>
2014-11-17 14:06:00 -0700

    Place Group Types

--- src/Rs/Client/PlaceGroupState.php
+++ src/Rs/Client/PlaceGroupState.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 
 namespace Gedcomx\Rs\Client;
 
+use Gedcomx\Gedcomx;
 use Guzzle\Http\Message\Request;
 use Guzzle\Http\Message\Response;
 
@@ -10,16 +11,34 @@ class PlaceGroupState extends GedcomxApplicationState
 
     protected function reconstruct(Request $request, Response $response)
     {
-        // TODO: Implement reconstruct() method.
+        return new PlaceGroupState($this->client, $request, $response, $this->accessToken, $this->stateFactory);
     }
 
     protected function loadEntity()
     {
-        // TODO: Implement loadEntity() method.
+        $json = json_decode($this->response->getBody(), true);
+        return new Gedcomx($json);
     }
 
     protected function getScope()
     {
-        // TODO: Implement getScope() method.
+        return $this->getPlaceGroup();
+    }
+
+    public function getSelfRel()
+    {
+        return Rel::PLACE_GROUP;
+    }
+
+    /**
+     * @return \Gedcomx\Source\SourceDescription|null
+     */
+    public function  getPlaceGroup()
+    {
+        if ($this->getEntity() != null) {
+            return $this->getEntity()->getSourceDescriptions();
+        }
+
+        return null;
     }
 }
