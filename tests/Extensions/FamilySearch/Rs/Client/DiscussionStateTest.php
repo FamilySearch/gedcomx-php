cf31f8890f0d18a195f99341b88e96f32b7d7e41
shane.walters@live.com <shane.walters@live.com>
2014-11-24 15:42:58 -0700

    Added read source description test.

--- tests/Extensions/FamilySearch/Rs/Client/DiscussionStateTest.php
+++ tests/Extensions/FamilySearch/Rs/Client/DiscussionStateTest.php
@@ -2,10 +2,18 @@
 
 namespace Gedcomx\Tests\Extensions\FamilySearch\Rs\Client;
 
+use Gedcomx\Common\Attribution;
+use Gedcomx\Common\Note;
+use Gedcomx\Common\ResourceReference;
+use Gedcomx\Common\TextValue;
 use Gedcomx\Extensions\FamilySearch\Platform\Discussions\Discussion;
 use Gedcomx\Extensions\FamilySearch\Rs\Client\DiscussionState;
 use Gedcomx\Extensions\FamilySearch\Rs\Client\FamilySearchStateFactory;
+use Gedcomx\Extensions\FamilySearch\Rs\Client\FamilyTree\FamilyTreeStateFactory;
+use Gedcomx\Rs\Client\SourceDescriptionState;
 use Gedcomx\Rs\Client\Util\HttpStatus;
+use Gedcomx\Source\SourceCitation;
+use Gedcomx\Source\SourceDescription;
 use Gedcomx\Tests\ApiTestCase;
 use Gedcomx\Tests\DiscussionBuilder;
 
@@ -218,4 +226,33 @@ class DiscussionStateTest extends ApiTestCase{
 
         $state->delete();
     }
-} 
+
+    public function testReadSourceDescription()
+    {
+        $this->collectionState(new FamilyTreeStateFactory());
+        $sd = new SourceDescription();
+        $citation = new SourceCitation();
+        $citation->setValue("\"United States Census, 1900.\" database and digital images, FamilySearch (https://familysearch.org/: accessed 17 Mar 2012), Ethel Hollivet, 1900; citing United States Census Office, Washington, D.C., 1900 Population Census Schedules, Los Angeles, California, population schedule, Los Angeles Ward 6, Enumeration District 58, p. 20B, dwelling 470, family 501, FHL microfilm 1,240,090; citing NARA microfilm publication T623, roll 90.");
+        $sd->setCitations(array($citation));
+        $title = new TextValue();
+        $title->setValue("1900 US Census, Ethel Hollivet");
+        $sd->setTitles(array($title));
+        $note = new Note();
+        $note->setText("Ethel Hollivet (line 75) with husband Albert Hollivet (line 74); also in the dwelling: step-father Joseph E Watkins (line 72), mother Lina Watkins (line 73), and grandmother -- Lina's mother -- Mary Sasnett (line 76).  Albert's mother and brother also appear on this page -- Emma Hollivet (line 68), and Eddie (line 69).");
+        $sd->setNotes(array($note));
+        $attribution = new Attribution();
+        $rr = new ResourceReference();
+        $rr->setResource("https://familysearch.org/platform/users/agents/MM6M-8QJ");
+        $rr->setResourceId("MM6M-8QJ");
+        $attribution->setContributor($rr);
+        $attribution->setModified(time());
+        $attribution->setChangeMessage("This is the change message");
+        $sd->setAttribution($attribution);
+        /** @var SourceDescriptionState $state */
+        $state = $this->collectionState()->addSourceDescription($sd)->get();
+
+        $this->assertNotNull($state->ifSuccessful());
+        $this->assertEquals(HttpStatus::OK, $state->getResponse()->getStatusCode());
+        $this->assertNotNull($state->getSourceDescription());
+    }
+}
