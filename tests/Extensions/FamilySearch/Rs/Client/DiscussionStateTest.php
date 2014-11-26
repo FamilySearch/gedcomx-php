25257ba83c6cc4880b6d121738c8c8a33de790b9
shane.walters@live.com <shane.walters@live.com>
2014-11-26 09:01:21 -0700

    Added memory comment tests.

--- tests/Extensions/FamilySearch/Rs/Client/DiscussionStateTest.php
+++ tests/Extensions/FamilySearch/Rs/Client/DiscussionStateTest.php
@@ -2,13 +2,17 @@
 
 namespace Gedcomx\Tests\Extensions\FamilySearch\Rs\Client;
 
+use Gedcomx\Extensions\FamilySearch\Platform\Discussions\Comment;
 use Gedcomx\Common\Attribution;
 use Gedcomx\Common\Note;
 use Gedcomx\Common\ResourceReference;
 use Gedcomx\Common\TextValue;
 use Gedcomx\Extensions\FamilySearch\Platform\Discussions\Discussion;
 use Gedcomx\Extensions\FamilySearch\Rs\Client\DiscussionState;
+use Gedcomx\Extensions\FamilySearch\Rs\Client\FamilySearchSourceDescriptionState;
 use Gedcomx\Extensions\FamilySearch\Rs\Client\FamilySearchStateFactory;
+use Gedcomx\Extensions\FamilySearch\Rs\Client\Memories\FamilySearchMemories;
+use Gedcomx\Rs\Client\Util\DataSource;
 use Gedcomx\Extensions\FamilySearch\Rs\Client\FamilyTree\FamilyTreeStateFactory;
 use Gedcomx\Rs\Client\SourceDescriptionState;
 use Gedcomx\Rs\Client\Util\HttpStatus;
@@ -17,7 +21,8 @@ use Gedcomx\Source\SourceDescription;
 use Gedcomx\Tests\ApiTestCase;
 use Gedcomx\Tests\DiscussionBuilder;
 
-class DiscussionStateTest extends ApiTestCase{
+class DiscussionStateTest extends ApiTestCase
+{
 
     /**
      * @link https://familysearch.org/developers/docs/api/discussions/Create_Discussion_usecase
@@ -183,7 +188,7 @@ class DiscussionStateTest extends ApiTestCase{
         $pass = false;
         $comments = $state->getDiscussion()->getComments();
         foreach ($comments as $c) {
-            if ($c->getId() == $comment->getId() & $c->getText() == $newText){
+            if ($c->getId() == $comment->getId() & $c->getText() == $newText) {
                 $pass = true;
             }
         }
@@ -226,4 +231,101 @@ class DiscussionStateTest extends ApiTestCase{
 
         $state->delete();
     }
-}
+
+    public function testCreateMemoriesComment()
+    {
+        $factory = new FamilySearchStateFactory();
+        /** @var FamilySearchMemories $memories */
+        $memories = $factory->newMemoriesState()
+            ->authenticateViaOAuth2Password(
+                $this->apiCredentials->username,
+                $this->apiCredentials->password,
+                $this->apiCredentials->apiKey);
+        $ds = new DataSource();
+        $ds->setTitle("Sample Memory");
+        $ds->setFile($this->makeTextFile());
+        /** @var FamilySearchSourceDescriptionState $artifact */
+        $artifact = $memories->addArtifact($ds)->get();
+        $comments = $artifact->readComments();
+        $comment = new Comment();
+        $comment->setText("Test comment.");
+        $state = $comments->addComment($comment);
+
+        $this->assertNotNull($state->ifSuccessful());
+        $this->assertEquals(HttpStatus::CREATED, $state->getResponse()->getStatusCode());
+
+        $artifact->delete();
+    }
+
+    public function testReadMemoriesComments()
+    {
+        $factory = new FamilySearchStateFactory();
+        /** @var FamilySearchMemories $memories */
+        $memories = $factory->newMemoriesState()
+            ->authenticateViaOAuth2Password(
+                $this->apiCredentials->username,
+                $this->apiCredentials->password,
+                $this->apiCredentials->apiKey);
+        $ds = new DataSource();
+        $ds->setTitle("Sample Memory");
+        $ds->setFile($this->makeTextFile());
+        /** @var FamilySearchSourceDescriptionState $artifact */
+        $artifact = $memories->addArtifact($ds)->get();
+        /** @var DiscussionState $state */
+        $comments = $artifact->readComments();
+        $comment = new Comment();
+        $comment->setText("Test comment.");
+        $comments->addComment($comment);
+        $state = $artifact->readComments();
+
+        $this->assertNotNull($state->ifSuccessful());
+        $this->assertEquals(HttpStatus::OK, $state->getResponse()->getStatusCode());
+        $this->assertNotNull($state->getDiscussion());
+        $this->assertNotNull($state->getDiscussion()->getComments());
+        $this->assertGreaterThan(0, count($state->getDiscussion()->getComments()));
+
+        $artifact->delete();
+    }
+
+    public function testUpdateMemoriesComment()
+    {
+        $factory = new FamilySearchStateFactory();
+        /** @var FamilySearchMemories $memories */
+        $memories = $factory->newMemoriesState()
+            ->authenticateViaOAuth2Password(
+                $this->apiCredentials->username,
+                $this->apiCredentials->password,
+                $this->apiCredentials->apiKey);
+        $ds = new DataSource();
+        $ds->setTitle("Sample Memory");
+        $ds->setFile($this->makeTextFile());
+        /** @var FamilySearchSourceDescriptionState $artifact */
+        $artifact = $memories->addArtifact($ds)->get();
+        $comments = $artifact->readComments();
+        $comment = new Comment();
+        $comment->setText("Test comment.");
+        $comments->addComment($comment);
+        $comments = $artifact->readComments();
+        $update = array_shift($comments->getDiscussion()->getComments());
+        $update->setText("Updated comment");
+        $state = $comments->updateComment($update);
+
+        $this->assertNotNull($state->ifSuccessful());
+        $this->assertEquals(HttpStatus::NO_CONTENT, $state->getResponse()->getStatusCode());
+
+        $artifact->delete();
+    }
+
+    private function makeTextFile()
+    {
+        $filename = 'test_' . bin2hex(openssl_random_pseudo_bytes(8)) . ".txt";
+
+        $file = file_put_contents($filename, join(" ", $this->faker->words()));
+        if (!$file) {
+            $this->fail("Failed to write test file.");
+        }
+
+
+        return $filename;
+    }
+} 
