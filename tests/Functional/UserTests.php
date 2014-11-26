26bc0ad54a024253eaf88c26026bcd4bceaca3c9
Sean Hammon <sean.hammon@stgconsulting.com>
2014-11-25 17:18:45 -0700

    Move User tests

--- tests/Functional/UserTests.php
+++ tests/Functional/UserTests.php
@@ -1,18 +1,36 @@
-<?php
+<?php 
 
-namespace Gedcomx\tests\Extensions\FamilySearch\Rs\Client;
+namespace Gedcomx\Tests\Functional;
 
-use Gedcomx\Extensions\FamilySearch\Rs\Client\FamilySearchCollectionState;
-use Gedcomx\Extensions\FamilySearch\Rs\Client\FamilySearchStateFactory;
-use Gedcomx\Records\Collection;
-use Gedcomx\Rs\Client\CollectionsState;
-use Gedcomx\Rs\Client\CollectionState;
-use Gedcomx\Rs\Client\GedcomxApplicationState;
-use Gedcomx\Rs\Client\Util\HttpStatus;
 use Gedcomx\Tests\ApiTestCase;
 
-class FamilySearchCollectionStateTest extends ApiTestCase
+class UserTests extends ApiTestCase
 {
+    /**
+     * @link https://familysearch.org/developers/docs/api/tree/Read_Current_Tree_Person_usecase
+     */
+    public function testReadCurrentTreePerson()
+    {
+        $collection = $this->collectionState(new StateFactory());
+        $personState = $collection->readPersonForCurrentUser();
+        /*
+         * readPersonForCurrentUser will return a 303 redirect by default.
+         * assert the URL on the person state is not the original request URL.
+         */
+        $this->assertFalse(strpos($personState->getResponse()->getEffectiveUrl(),Rel::CURRENT_USER_PERSON));
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/tree/Read_Current_Tree_Person_Expecting_200_Response_usecase
+     */
+    public function testReadCurrentTreePersonExpecting200()
+    {
+        $collection = $this->collectionState(new StateFactory());
+        $expect200 = new HeaderParameter(true,"Expect",200);
+        $personState = $collection->readPersonForCurrentUser($expect200);
+        $this->assertEquals(HttpStatus::OK, $personState->getResponse()->getStatusCode());
+    }
+
     /**
      * @link https://familysearch.org/developers/docs/api/tree/Read_Current_User_usecase
      */
@@ -38,6 +56,14 @@ class FamilySearchCollectionStateTest extends ApiTestCase
             $this->buildFailMessage(__METHOD__, $historyState)
         );
         $this->assertNotEmpty($historyState->getUserHistory());
+    }
+
+    /**
+     * @link https://familysearch.org/developers/docs/api/tree/Read_User_usecase
+     */
+    public function testReadUser()
+    {
+        $this->markTestIncomplete('Not yet implemented.');
     }
 
     /**
