c5ebab066d437f27239ccaedf1dd99bb5ef5ec83
Sean Hammon <sean.hammon@stgconsulting.com>
2014-12-08 12:13:22 -0700

    A quick untested implementation of a default JSON deserializer

--- src/GedcomxFile/DefaultJsonSerialization.php
+++ src/GedcomxFile/DefaultJsonSerialization.php
@@ -2,6 +2,59 @@
 
 namespace Gedcomx\GedcomxFile;
 
-class DefaultJsonSerialization {
+use Gedcomx\Extensions\FamilySearch\FamilySearchPlatform;
+use Gedcomx\Gedcomx;
 
-}
+class DefaultJsonSerialization implements GedcomxEntrySerializer, GedcomxEntryDeserializer
+{
+    /**
+     * Deserialize the resource from the specified input stream.
+     *
+     * @param string $incoming The text to deserialize.
+     *
+     * @return mixed the resource.
+     */
+    public function deserialize($incoming)
+    {
+        $resources = null;
+
+        $json = json_decode($incoming);
+        foreach ($json as $key => $value){
+            if (JsonMapper::isKnownType($key)) {
+                $class = XmlMapper::getClassName($key);
+                $resources[] = new $class($value);
+            }
+        }
+
+        return $resources;
+
+    }
+
+    /**
+     * Serialize the resource to the specified output stream.
+     *
+     * @param mixed $resource
+     *
+     * @return void
+     */
+    public function serialize($resource)
+    {
+        return $resource->toJson();
+    }
+
+    /**
+     * Whether the specified content type is a known content type and therefore
+     * does not need to be written to the entry attributes.
+     *
+     * @param string $contentType The content type.
+     *
+     * @return boolean Whether the content type is "known".
+     */
+    public function isKnownContentType($contentType)
+    {
+        return in_array($contentType, array(
+            Gedcomx::JSON_MEDIA_TYPE,
+            FamilySearchPlatform::JSON_MEDIA_TYPE,
+            FamilySearchPlatform::JSON_APPLICATION_TYPE
+        ));
+}}
