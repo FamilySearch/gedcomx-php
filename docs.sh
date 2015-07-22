f6717f7e02447cd2a2d7649c50b4d7f659d3f7fa
Justin <justincyork@gmail.com>
2015-07-22 21:36:01 +0000

    try updating docs incrementally

--- docs.sh
+++ docs.sh
@@ -2,22 +2,27 @@
 wget http://www.apigen.org/apigen.phar
 
 # Get the boostrap theme
-git clone https://github.com/jimmyz/ThemeBootstrap.git
+git clone https://github.com/jimmyz/ThemeBootstrap.git ../ThemeBootstrap
 
-# Generate Api
-php apigen.phar generate -s src -d ../gh-pages --access-levels="public" --title="gedcomx-php" --template-config="ThemeBootstrap/src/config.neon"
-cd ../gh-pages
+# Generate docs
+php apigen.phar generate -s src -d ../docs --access-levels="public" --title="gedcomx-php" --template-config="../ThemeBootstrap/src/config.neon"
 
 # Set identity
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
-# Add branch
-git init
-git remote add origin https://${GH_TOKEN}@github.com/FamilySearch/gedcomx-php.git > /dev/null
-git checkout -B gh-pages
+# Switch to gh-pages
+git checkout gh-pages
+
+# Clear the current directory so that we don't accidentally
+# commit any leftover files from the build or test process
+# such as the vendor folder
+rm -rf ./*
+
+# Delete old docs and copy in new docs
+cp -R ../docs/* ./
 
 # Push generated files
 git add .
 git commit -m "Update docs"
-git push origin gh-pages -fq > /dev/null
+git push origin gh-pages -q > /dev/null
