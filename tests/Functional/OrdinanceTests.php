cc7cc3ee7731c65f3d6acd29218a8537f24f6310
Justin <justincyork@gmail.com>
2015-07-30 16:06:47 +0000

    ordinance tests

--- tests/Functional/OrdinanceTests.php
+++ tests/Functional/OrdinanceTests.php
@@ -7,7 +7,6 @@ use Gedcomx\Rs\Client\GedcomxApplicationState;
 use Gedcomx\Rs\Client\StateFactory;
 use Gedcomx\Rs\Client\Util\HttpStatus;
 use Gedcomx\Tests\SandboxCredentials;
-use Guzzle\Http\Message\Request;
 use Gedcomx\Tests\ApiTestCase;
 
 class OrdinanceTests extends ApiTestCase
@@ -22,10 +21,12 @@ class OrdinanceTests extends ApiTestCase
     public function testReadOrdinancePolicy()
     {
         $this->collectionState(new StateFactory());
-        $request = $this->collectionState()->getClient()->createRequest(Request::GET, $this->policyUri);
-        $request->addHeader('Authorization', "Bearer " . $this->collectionState()->getAccessToken());
-        $request->setHeader('Accept', Gedcomx::PLAIN_TEXT);
-        $response = $request->send();
+        $response = $this->collectionState()->getClient()->get($this->policyUri, [
+            'headers' => [
+                'Authorization' => "Bearer " . $this->collectionState()->getAccessToken(),
+                'Accept' => Gedcomx::PLAIN_TEXT
+            ]
+        ]);
         $this->assertEquals(
             HttpStatus::OK,
             $response->getStatusCode(),
@@ -33,7 +34,7 @@ class OrdinanceTests extends ApiTestCase
         );
         $this->assertContains(
             "Who You Can Do Ordinances For",
-            $response->getBody(true),
+            (string) $response->getBody(),
             "Response doesn't appear to be in English."
         );
     }
@@ -45,11 +46,13 @@ class OrdinanceTests extends ApiTestCase
     public function testReadOrdinancePolicyInFrench()
     {
         $this->collectionState(new StateFactory());
-        $request = $this->collectionState()->getClient()->createRequest(Request::GET, $this->policyUri);
-        $request->addHeader('Authorization', "Bearer " . $this->collectionState()->getAccessToken());
-        $request->setHeader('Accept', Gedcomx::PLAIN_TEXT);
-        $request->setHeader('Accept-Language', "fr");
-        $response = $request->send();
+        $response = $this->collectionState()->getClient()->get($this->policyUri, [
+            'headers' => [
+                'Authorization' => "Bearer " . $this->collectionState()->getAccessToken(),
+                'Accept' => Gedcomx::PLAIN_TEXT,
+                'Accept-Language' => 'fr'
+            ]
+        ]);
         $this->assertEquals(
             HttpStatus::OK,
             $response->getStatusCode(),
@@ -57,7 +60,7 @@ class OrdinanceTests extends ApiTestCase
         );
         $this->assertContains(
             "Personnes pour lesquelles vous pouvez accomplir des ordonnances",
-            $response->getBody(true),
+            (string) $response->getBody(),
             "Response doesn't appear to be in French."
         );
     }
@@ -69,11 +72,13 @@ class OrdinanceTests extends ApiTestCase
     public function testReadOrdinances()
     {
         $this->collectionState(new StateFactory());
-        $request = $this->collectionState()->getClient()->createRequest(Request::GET, $this->ordinanceUri);
-        $request->addHeader('Authorization', "Bearer " . $this->collectionState()->getAccessToken());
-        $request->setHeader('Accept', Gedcomx::JSON_MEDIA_TYPE);
-        $request->setHeader('Content-Type', Gedcomx::JSON_MEDIA_TYPE);
-        $response = $request->send();
+        $response = $this->collectionState()->getClient()->get($this->ordinanceUri, [
+            'headers' => [
+                'Authorization' => "Bearer " . $this->collectionState()->getAccessToken(),
+                'Accept' => Gedcomx::JSON_MEDIA_TYPE,
+                'Content-Type' => Gedcomx::JSON_MEDIA_TYPE
+            ]
+        ]);
         $this->assertEquals(
             HttpStatus::OK,
             $response->getStatusCode(),
@@ -101,11 +106,13 @@ class OrdinanceTests extends ApiTestCase
                 'general_public_user',
                 '1234pass',
                 SandboxCredentials::API_KEY);
-        $request = $collectionState->getClient()->createRequest(Request::GET, $this->ordinanceUri);
-        $request->addHeader('Authorization', "Bearer " . $collectionState->getAccessToken());
-        $request->setHeader('Accept', Gedcomx::JSON_MEDIA_TYPE);
-        $request->setHeader('Content-Type', Gedcomx::JSON_MEDIA_TYPE);
-        $response = $request->send();
+        $response = $collectionState->getClient()->get($this->ordinanceUri, [
+            'headers' => [
+                'Authorization' => "Bearer " . $collectionState->getAccessToken(),
+                'Accept' => Gedcomx::JSON_MEDIA_TYPE,
+                'Content-Type' => Gedcomx::JSON_MEDIA_TYPE
+            ]
+        ]);
         $this->assertEquals(
             HttpStatus::FORBIDDEN,
             $response->getStatusCode(),
